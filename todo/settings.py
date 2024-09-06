aa423c709ae7735f373a581625254b9abda01150
vinayvasantham <111839456+Vinayvasantham@users.noreply.github.com>
2024-09-06 19:59:15 +0530

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 import django_on_heroku
-import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
